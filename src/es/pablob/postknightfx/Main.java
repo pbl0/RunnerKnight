b9a36661a43392d09e3a835869e8fd535d67a86e
PC15 <PC15@DESKTOP-I04G3ED>
2019-01-10 10:33:07 +0100

    dibujar bola, se mueve en eje x y rebota en las paredes.

--- src/es/pablob/postknightfx/Main.java
+++ src/es/pablob/postknightfx/Main.java
@@ -5,12 +5,15 @@
  */
 package es.pablob.postknightfx;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+//import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -19,26 +22,37 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
     
+    int ballCenterX = 10;
+    int ballCurrentSpeedX = 3;
+    
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Hola Hola Hola");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
+   public void start(Stage primaryStage) {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 640, 480, Color.BLACK);
         primaryStage.setTitle("Postknight");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        Circle circleBall = new Circle(ballCenterX, 30, 7, Color.WHITE);
+        root.getChildren().add(circleBall);
+        
+        AnimationTimer animationBall = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                circleBall.setCenterX(ballCenterX);
+                ballCenterX += ballCurrentSpeedX;
+                if(ballCenterX >= 640) {
+                    ballCurrentSpeedX = -3;
+                }
+                else if (ballCenterX <= 0) {
+                    ballCurrentSpeedX = 3;
+                }
+                //System.out.println(ballCenterX);
+               
+            };
+        };
+        animationBall.start();
+        
     }
 
     /**
