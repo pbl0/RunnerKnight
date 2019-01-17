7827e5e82b743d600609e05443bdfb998d2d2b43
PC15 <PC15@DESKTOP-I04G3ED>
2019-01-17 10:36:11 +0100

    Fondo no se corta al volver hacia atrás. Animación de atacar -> tecla "A".

--- src/es/pablob/postknightfx/Main.java
+++ src/es/pablob/postknightfx/Main.java
@@ -7,6 +7,7 @@ package es.pablob.postknightfx;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -15,9 +16,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.shape.Rectangle;
 //import javafx.scene.shape.Circle;
-//import javafx.scene.Group;
 
 /**
  *
@@ -33,13 +32,15 @@ public class Main extends Application {
     int speed = 2;
     int heroeWidht = 88;
     int heroeHeight = 64;
-    int heroeX = 200;
-    int heroeY = 230;
+    int heroeX = 2;
+    int heroePosX = 200;
+    int heroeY = 270;
+    int heroeSpeed = 0;
     
     @Override
     public void start(Stage primaryStage) {
         Pane root = new Pane();
-        Scene scene = new Scene(root, windowWidth, windowHeight, Color.LIGHTGREEN);
+        Scene scene = new Scene(root, windowWidth, windowHeight, Color.CYAN);
         primaryStage.setTitle("Postknight");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -49,35 +50,49 @@ public class Main extends Application {
         ImageView bg1 = new ImageView(bg);
         ImageView bg2 = new ImageView(bg);
         
-        //Personaje corriendo
+        bg1.setFitHeight(root.getHeight());
+        bg2.setFitHeight(root.getHeight());
+        
+        //heroe corriendo
         Image runGif = new Image(getClass().getResourceAsStream("images/run.gif"));
         ImageView run = new ImageView(runGif);
         run.setFitWidth(heroeWidht);
         run.setFitHeight(heroeHeight);
-        run.setX(heroeX);
-        run.setY(heroeY);
         run.setVisible(false);
         
-        //Personaje parado
+        //heroe parado
         Image idleGif = new Image(getClass().getResourceAsStream("images/idle.gif"));
         ImageView idle = new ImageView(idleGif);
         idle.setFitHeight(heroeHeight);
         idle.setFitWidth(heroeWidht);
-        idle.setX(heroeX);
-        idle.setY(heroeY);
+
         
+        //heroe atacando
+        Image attackGif = new Image(getClass().getResourceAsStream("images/attack.gif"));
+        ImageView attack = new ImageView(attackGif);
+        attack.setFitHeight(heroeHeight);
+        attack.setFitWidth(heroeWidht);
+        attack.setVisible(false);
+        
+        //grupo heroe
+        Group groupHeroe = new Group(run, idle, attack);
+        groupHeroe.setLayoutX(heroeX);
+        groupHeroe.setLayoutY(heroeY);
         
         //Rectangle rectanglePersonaje = new Rectangle(200,230,64,64);
         //rectanglePersonaje.setFill(Color.TRANSPARENT);
         
-        root.getChildren().addAll(bg1,bg2,idle,run);
-
+        root.getChildren().addAll(bg1,bg2,groupHeroe);
+        
+        
         AnimationTimer animationBG = new AnimationTimer() {
             @Override
             public void handle(long now) { 
                 bg1.setX(bg1X);
                 bg2.setX(bg2X);
-                
+
+                groupHeroe.setLayoutX(heroeX);
+                heroeX -= heroeSpeed;
                 bg1X -= bgCurrentSpeed;
                 bg2X -= bgCurrentSpeed;
                 
@@ -86,17 +101,14 @@ public class Main extends Application {
                     bg1X = bgWidth;
                     bg1.setX(bg1X);
                     root.getChildren().add(bg1);
-                    run.toFront();
-                    idle.toFront();
+                    groupHeroe.toFront();
                     
                 } else if (bg2X == -bgWidth) {
                     root.getChildren().remove(bg2);
                     bg2X = bgWidth;
                     bg2.setX(bg2X);
                     root.getChildren().add(bg2);
-                    run.toFront();
-                    idle.toFront();
-                    
+                    groupHeroe.toFront();  
                 }
             };
         };
@@ -106,32 +118,66 @@ public class Main extends Application {
         //Teclado
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
             if(key.getCode()==KeyCode.RIGHT) {
-                run.setScaleX(1);
+                groupHeroe.setScaleX(1);
                 idle.setVisible(false);
                 run.setVisible(true);
-                bgCurrentSpeed = speed;
+                attack.setVisible(false);
+                if (heroeX <= heroePosX) {
+                    bgCurrentSpeed = 0;
+                    heroeSpeed =-2;
+                } else {
+                    heroeSpeed = 0;
+                    bgCurrentSpeed = speed; 
+                }
             }   
         });
         scene.addEventHandler(KeyEvent.KEY_RELEASED, (key) -> {
             if(key.getCode()==KeyCode.RIGHT) {
                 idle.setVisible(true);
                 run.setVisible(false);
+                
                 bgCurrentSpeed = 0;
+                heroeSpeed = 0;
             }   
         });
-                scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
             if(key.getCode()==KeyCode.LEFT) {
-                run.setScaleX(-1);
+                groupHeroe.setScaleX(-1);
                 idle.setVisible(false);
                 run.setVisible(true);
-                bgCurrentSpeed = -speed;
-            }   
+                attack.setVisible(false);
+                bgCurrentSpeed = 0;
+                if (heroeX <= 2){
+                    heroeSpeed = 0;
+                } else {
+                    heroeSpeed = 2;
+                }  
+            };   
         });
         scene.addEventHandler(KeyEvent.KEY_RELEASED, (key) -> {
             if(key.getCode()==KeyCode.LEFT) {
                 idle.setVisible(true);
                 run.setVisible(false);
                 bgCurrentSpeed = 0;
+                heroeSpeed = 0;
+            }   
+        });
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if(key.getCode()==KeyCode.A) {
+                idle.setVisible(false);
+                run.setVisible(false);
+                attack.setVisible(true);
+                bgCurrentSpeed = 0;
+                heroeSpeed = 0;
+            }   
+        });
+        scene.addEventHandler(KeyEvent.KEY_RELEASED, (key) -> {
+            if(key.getCode()==KeyCode.A) {
+                idle.setVisible(true);
+                run.setVisible(false);
+                attack.setVisible(false);
+                bgCurrentSpeed = 0;
+                heroeSpeed = 0;
             }   
         });
         
